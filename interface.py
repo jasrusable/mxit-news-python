8240ff0630027a41a4ec6156797e45672180faf5
Pierre Hugo <avoid3d@gmail.com>
2013-07-13 16:37:01 +0200

    refactored interface

--- interface.py
+++ interface.py
@@ -12,50 +12,46 @@ article_parser = ArticleParser()
 
 app = Flask(__name__)
 
+def search(query):
+    """Respond with a list of search results.
+    """
+
+    results = news_searcher.search(query)
+    return render_template('search_results.html', results=results, query=query)
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    """News homepage, list news topics and link to search.
+    """Homepage, list news topics or respond with search results if
+    X-Mxit-User-Input is present.
     """
     query = request.form.get('X-Mxit-User-Input')
     if query:
-        results = google_news_search.search(query)
-        return render_template('search_results.html', results=results, query=query)
+        # The user has submitted a search query.
+        return search(query)
+    else:
+        # The user is requesting the home page.
+        topics = news_browser.get_topics()
+        return render_template('index.html',
+                topics=topics,
+                debug=True)
 
-    topics = google_news.get_topics()
-    return render_template('index.html',
-            topics=topics,
-            debug=True)
+@app.route('/topic/')
+def topic():
+    topic_id = request.args.get('id')
+    topic = news_browser.fetch_topic(topic_id)
 
-@app.route('/news/topic/<topic_id>')
-def topic(topic_id):
-    articles = google_news.get_articles(topic_id)
-    topic_name = google_news.topics[topic_id]['name']
-    return render_template('topic.html',
-            back_url=url_for('index'),
-            articles=articles,
-            topic_id=topic_id,
-            topic_name=topic_name)
-
-@app.route('/search_article/<article_url>')
-def search_article(article_url):
-    url = b64decode(article_url.encode('utf-8'))
-    url = urlparse.decode(url)
-    title, text = article_parser.fetch_article(url)
-    return render_template('search_article.html',
-            text=text,
-            title=title
-            )
+    return render_template('topic.html', topic=topic)
 
 
-@app.route('/news/<topic_id>/article/<article_url>')
-def article(article_url, topic_id):
-    url = b64decode(article_url.encode('utf-8'))
-    title, text = article_parser.fetch_article(url)
-    return render_template('article.html',
-            text=text,
-            title=title,
-            back_url=url_for('topic', topic_id=topic_id))
+@app.route('/article/')
+def article():
+    b64_url = request.args.get('b64_url')
+    url = b64decode(b64_url.encode('ascii'))
+    article = article_reader.fetch_article(url)
+
+    return render_template('article.html',article=article)
 
 if __name__ == '__main__':
 	app.run(debug=True)

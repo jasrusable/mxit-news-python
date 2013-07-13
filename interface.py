36b7f3d6202d844321a28d69f9aea0d2f4cc8183
Pierre Hugo <avoid3d@gmail.com>
2013-07-13 20:33:36 +0200

    Added proper back functionality.

--- interface.py
+++ interface.py
@@ -3,7 +3,7 @@ from flask import Flask, url_for, render_template, redirect, request
 from base64 import urlsafe_b64decode as b64decode
 
 from backend import GoogleNewsSearch, GoogleNews, ArticleReader
-from util import cached_call
+from util import cached_call, get_with_back
 
 news_searcher = GoogleNewsSearch()
 news_browser = GoogleNews()
@@ -14,9 +14,10 @@ app = Flask(__name__)
 def search(query):
     """Respond with a list of search results.
     """
-
     results = cached_call(news_searcher.search, query=query)
-    return render_template('search_results.html', results=results, query=query)
+    return render_template('search_results.html', results=results, query=query,
+            with_back=get_with_back(request),
+            back_url=request.args.get('back_url'))
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -33,20 +34,28 @@ def index():
         topics = news_browser.topics
         return render_template('index.html',
                 topics=topics,
-                debug=True)
+                debug=True,
+                with_back=get_with_back(request),
+                back_url=request.args.get('back_url'))
 
 @app.route('/topic/')
 def topic():
     topic_id = request.args.get('id')
+    print(request.args)
     topic = cached_call(news_browser.fetch_topic, topic_id)
-    return render_template('topic.html', topic=topic)
+    return render_template('topic.html', topic=topic, with_back=get_with_back(request),
+            back_url=request.args.get('back_url'))
 
 
 @app.route('/article/')
 def article():
+    print(request.args)
     url = request.args.get('url')
     article = cached_call(article_reader.fetch_article, url)
-    return render_template('article.html',article=article)
+    return render_template('article.html',
+            article=article,
+            with_back=get_with_back(request),
+            back_url=request.args.get('back_url'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- util.py
+++ util.py
@@ -1,6 +1,29 @@
 import memcache
+import urlparse
+import urllib
 
 client = memcache.Client(['localhost:11211'], debug=True)
+
+def get_with_back(request):
+    def _with_back(url):
+        url_parts = urlparse.urlparse(url)
+        query_str = url_parts.query
+        query = urlparse.parse_qs(query_str)
+        new_query = {}
+        for k, v in query.iteritems():
+            new_query[k] = v[0]
+        new_query['back_url'] = request.url
+        query_str = urllib.urlencode(new_query)
+
+        url_parts = (
+                url_parts.scheme,
+                url_parts.netloc,
+                url_parts.path,
+                url_parts.params,
+                query_str,
+                url_parts.fragment)
+        return urlparse.urlunparse(url_parts)
+    return _with_back
 
 def cached_call(f, *args, **kwargs):
     key = str(hash(hash(frozenset(kwargs.items())) + hash(args)))
